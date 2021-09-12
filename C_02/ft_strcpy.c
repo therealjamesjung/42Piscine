fd2881eff5d0be5d672da8c674dcd12970b29aa1
James Jung <therealjamesjung@gmail.com>
2021-09-12 16:57:44 +0900

    Lint with norminette

--- C_02/ft_strcpy.c
+++ C_02/ft_strcpy.c
@@ -6,23 +6,19 @@
 /*   By: jaekjung <jaekjung@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/12 16:36:39 by jaekjung          #+#    #+#             */
-/*   Updated: 2021/09/12 16:52:09 by jaekjung         ###   ########.fr       */
+/*   Updated: 2021/09/12 16:57:31 by jaekjung         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-
 char	*ft_strcpy(char *dest, char *src)
 {
-	int index;
+	int	index;
 
 	index = 0;
-	
 	while (src[index] != '\0')
 	{
 		dest[index] = src[index];
 		index++;
 	}
-
 	return (dest);
 }
