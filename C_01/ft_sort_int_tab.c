bef5d1dbf3f884f95317d27b238d4e3b2dfa5a2b
James Jung <therealjamesjung@gmail.com>
2021-09-12 16:41:10 +0900

    Lint with norminette

--- C_01/ft_sort_int_tab.c
+++ C_01/ft_sort_int_tab.c
@@ -6,7 +6,7 @@
 /*   By: jaekjung <jaekjung@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/12 16:23:17 by jaekjung          #+#    #+#             */
-/*   Updated: 2021/09/12 16:34:19 by jaekjung         ###   ########.fr       */
+/*   Updated: 2021/09/12 16:40:22 by jaekjung         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,18 +22,17 @@ void	ft_swap(int *a, int *b)
 void	ft_sort_int_tab(int *tab, int size)
 {
 	int	i;
-	int j;
+	int	j;
 
 	i = 0;
 	j = 0;
-
 	while (i < size - 1)
 	{
 		j = 0;
 		while (j < size - 1)
 		{
 			if (tab[j] > tab[j + 1])
-				ft_swap(&tab[j], &tab[j + 1]);	
+				ft_swap(&tab[j], &tab[j + 1]);
 			j++;
 		}
 		i++;
