3969a4b76395a8b863ea794b2d2e431c4b8d0c06
James Jung <therealjamesjung@gmail.com>
2021-09-12 17:00:43 +0900

    ft_strncpy.c

---
+++ C_02/ft_strncpy.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaekjung <jaekjung@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/12 16:57:55 by jaekjung          #+#    #+#             */
+/*   Updated: 2021/09/12 16:59:45 by jaekjung         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int	index;
+
+	index = 0;
+	while (index < n)
+	{
+		dest[index] = src[index];
+		index++;
+	}
+	return (dest);
+}
