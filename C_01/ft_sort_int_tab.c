6ac759c8c02d0fb7aa7418fdeae0e50bf196a0c2
James Jung <therealjamesjung@gmail.com>
2021-09-12 16:34:35 +0900

    ft_sort_int_tab.c

---
+++ C_01/ft_sort_int_tab.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaekjung <jaekjung@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/12 16:23:17 by jaekjung          #+#    #+#             */
+/*   Updated: 2021/09/12 16:34:19 by jaekjung         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_swap(int *a, int *b)
+{
+	int	tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	i;
+	int j;
+
+	i = 0;
+	j = 0;
+
+	while (i < size - 1)
+	{
+		j = 0;
+		while (j < size - 1)
+		{
+			if (tab[j] > tab[j + 1])
+				ft_swap(&tab[j], &tab[j + 1]);	
+			j++;
+		}
+		i++;
+	}
+}
