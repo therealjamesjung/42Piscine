648ac643490241b365e66ac38910dff377663571
James Jung <therealjamesjung@gmail.com>
2021-09-12 17:42:53 +0900

    ft_strlowcase.c

---
+++ C_02/ft_strlowcase.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlowcase.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaekjung <jaekjung@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/12 17:41:37 by jaekjung          #+#    #+#             */
+/*   Updated: 2021/09/12 17:42:05 by jaekjung         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strupcase(char *str)
+{
+	int	index;
+
+	index = 0;
+	while (str[index] != '\0')
+	{
+		if (str[index] >= 'A' && str[index <= 'Z'])
+			str[index] = str[index] + 'a' - 'A';
+		index++;
+	}
+	return (str);
+}
