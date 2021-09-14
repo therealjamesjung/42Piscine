55285fa6d19197be4394c8e1b5ae650d88bc1c52
James Jung <therealjamesjung@gmail.com>
2021-09-14 22:56:17 +0900

    C_00/ft_putnbr.c

--- C_00/ft_putnbr.c
+++ C_00/ft_putnbr.c
@@ -3,19 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putnbr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jaekjung <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: jaekjung <jaekjung@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/11 16:23:58 by jaekjung          #+#    #+#             */
-/*   Updated: 2021/09/11 16:24:40 by jaekjung         ###   ########.fr       */
+/*   Updated: 2021/09/14 22:22:17 by jaekjung         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
 
-int _get_digits(int n)
+int	_get_digits(int n)
 {
-	int digit;
+	int	digit;
 
 	digit = 1;
 	while (n >= 10)
@@ -23,14 +23,13 @@ int _get_digits(int n)
 		digit *= 10;
 		n /= 10;
 	}
-
 	return (digit);
 }
 
 void	ft_putnbr(int nb)
 {
 	char	c;
-	char 	tmp;
+	char	tmp;
 	int		digit;
 
 	c = '0';
@@ -39,6 +38,11 @@ void	ft_putnbr(int nb)
 		tmp = '-';
 		write (1, &tmp, 1);
 		nb *= -1;
+	}
+	else if (nb == 0)
+	{
+		write(1, "0", 1);
+		return ;
 	}
 	digit = _get_digits(nb);
 	while (digit > 0)
