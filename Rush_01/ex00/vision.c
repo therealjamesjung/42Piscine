94916efbf65ff19d2d90e430b24c728ad6185994
James Jung <therealjamesjung@gmail.com>
2021-09-19 21:24:53 +0900

    Norminette on vision.c

--- Rush_01/ex00/vision.c
+++ Rush_01/ex00/vision.c
@@ -6,13 +6,12 @@
 /*   By: jaekjung <jaekjung@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/19 00:19:46 by jaekjung          #+#    #+#             */
-/*   Updated: 2021/09/19 13:44:23 by jaekjung         ###   ########.fr       */
+/*   Updated: 2021/09/19 21:24:50 by jaekjung         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "data.h"
 #include <unistd.h>
-#include <stdio.h>
 #include <stdlib.h>
 
 int	_check_row_l(int **map, t_data input, t_point pnt)
@@ -23,8 +22,8 @@ int	_check_row_l(int **map, t_data input, t_point pnt)
 
 	cnt = 1;
 	i = 0;
-	tmp = map[pnt.y][i];
-	while (i < input.n)
+	tmp = map[pnt.y][i--];
+	while (++i < input.n)
 	{
 		if (map[pnt.y][i] != 0)
 		{
@@ -34,8 +33,7 @@ int	_check_row_l(int **map, t_data input, t_point pnt)
 				cnt++;
 			}
 			if (map[pnt.y][i] == input.n)
-				break;
-			i++;
+				break ;
 		}
 		else
 			return (1);
@@ -53,8 +51,8 @@ int	_check_row_r(int **map, t_data input, t_point pnt)
 
 	cnt = 1;
 	i = input.n - 1;
-	tmp = map[pnt.y][i];
-	while (i >= 0)
+	tmp = map[pnt.y][i++];
+	while (--i >= 0)
 	{
 		if (map[pnt.y][i] != 0)
 		{
@@ -64,8 +62,7 @@ int	_check_row_r(int **map, t_data input, t_point pnt)
 				cnt++;
 			}
 			if (map[pnt.y][i] == input.n)
-				break;
-			i--;
+				break ;
 		}
 		else
 			return (1);
@@ -83,8 +80,8 @@ int	_check_col_u(int **map, t_data input, t_point pnt)
 
 	cnt = 1;
 	i = 0;
-	tmp = map[i][pnt.x];
-	while (i < input.n)
+	tmp = map[i--][pnt.x];
+	while (++i < input.n)
 	{
 		if (map[i][pnt.x] != 0)
 		{
@@ -94,8 +91,7 @@ int	_check_col_u(int **map, t_data input, t_point pnt)
 				cnt++;
 			}
 			if (map[i][pnt.x] == input.n)
-				break;
-			i++;
+				break ;
 		}
 		else
 			return (1);
@@ -113,8 +109,8 @@ int	_check_col_d(int **map, t_data input, t_point pnt)
 
 	cnt = 1;
 	i = input.n - 1;
-	tmp = map[i][pnt.x];
-	while (i >= 0)
+	tmp = map[i++][pnt.x];
+	while (--i >= 0)
 	{
 		if (map[i][pnt.x] != 0)
 		{
@@ -124,8 +120,7 @@ int	_check_col_d(int **map, t_data input, t_point pnt)
 				cnt++;
 			}
 			if (map[i][pnt.x] == input.n)
-				break;
-			i--;
+				break ;
 		}
 		else
 			return (1);
@@ -135,7 +130,7 @@ int	_check_col_d(int **map, t_data input, t_point pnt)
 	return (1);
 }
 
-int _check_vis(int **map, t_data input, t_point pnt)
+int	_check_vis(int **map, t_data input, t_point pnt)
 {
 	if (!_check_col_u(map, input, pnt))
 		return (0);
