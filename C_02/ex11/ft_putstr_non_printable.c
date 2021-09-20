5ffeeb2dcd705844d873a32bef2af1389d4af58f
jaekjung <therealjamesjung@gmail.com>
2021-09-20 15:06:45 +0900

    Add edge cases on char input with neg values

--- C_02/ex11/ft_putstr_non_printable.c
+++ C_02/ex11/ft_putstr_non_printable.c
@@ -6,7 +6,7 @@
 /*   By: jaekjung <jaekjung@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/12 18:18:46 by jaekjung          #+#    #+#             */
-/*   Updated: 2021/09/15 12:59:10 by jaekjung         ###   ########.fr       */
+/*   Updated: 2021/09/20 15:05:46 by jaekjung         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,19 +26,30 @@ int	_is_prtble(char c)
 
 void	ft_putstr_non_printable(char *str)
 {
-	int		index;
+	int				index;
+	unsigned char	tmp;
 
 	index = 0;
 	while (str[index] != '\0')
 	{
-		if (_is_prtble(str[index]) == 1)
-			ft_putchar(str[index]);
+		tmp = (unsigned char) str[index];
+		if (_is_prtble(tmp) == 1)
+			ft_putchar(tmp);
 		else
 		{
 			ft_putchar('\\');
-			ft_putchar("0123456789abcdef"[str[index] / 16]);
-			ft_putchar("0123456789abcdef"[str[index] % 16]);
+			ft_putchar("0123456789abcdef"[tmp / 16]);
+			ft_putchar("0123456789abcdef"[tmp % 16]);
 		}
 		index++;
 	}
+}
+
+#include <stdio.h>
+
+int main()
+{
+	ft_putstr_non_printable("Coucou\ntu vas bien ?");
+
+	return 0;
 }
